80fe044dce38fbb182a9825e5c14c1b4b9704965
Shiraz Adam <shiraz0@outlook.com>
2022-01-26 14:21:11 +0000

    UI to download selected stream
    Updated the code to be able to download the selected stream via Combo Box

--- Main.py
+++ Main.py
@@ -14,10 +14,14 @@ from pytube import YouTube
 from pytube import Stream
 from tkinter import filedialog
 from tkinter import messagebox
+import os
 
 class MainForm(Form):
-
     def __init__(self, owner) -> None:
+        self.StreamID : str = "Stream ID:"
+        self.ITag : int = 0
+        self.YT : YouTube()
+
         self.Caption = "Delphi Python YouTube Downloader"
         self.SetBounds(10, 10, 600, 400)
         self.Position = "poScreenCenter"
@@ -26,10 +30,10 @@ class MainForm(Form):
         self.lblInstructions.SetProps(Parent = self, Caption = "Enter the URL below to download the video")
         self.lblInstructions.SetBounds(10, 10, 300, 24)
 
-        self.txtEdit = Edit(self)
-        self.txtEdit.SetProps(Parent = self)
-        self.txtEdit.SetBounds(10, 30, 420, 20)
-        self.txtEdit.Text = "https://www.youtube.com/watch?v=di3fLHgey8U"
+        self.txtEditUrl = Edit(self)
+        self.txtEditUrl.SetProps(Parent = self)
+        self.txtEditUrl.SetBounds(10, 30, 420, 20)
+        self.txtEditUrl.Text = "https://www.youtube.com/watch?v=SogtUeGKm4s"
 
         self.btnAnalyse = Button(self)
         self.btnAnalyse.SetProps(Parent = self, Caption = "Analyse")
@@ -48,6 +52,7 @@ class MainForm(Form):
         self.cmbBxVideos = ComboBox(self)
         self.cmbBxVideos.SetProps(Parent = self)
         self.cmbBxVideos.SetBounds(10, 95, 560, 25)
+        self.cmbBxVideos.OnChange = self.__on_cmbBxVideos_Change
 
         self.btnDownload = Button(self)
         self.btnDownload.SetProps(Parent = self, Caption = "Download")
@@ -61,43 +66,52 @@ class MainForm(Form):
         self.OnClose = self.__on_form_close
 
     def __on_btnAnalyse_Click(self, sender) -> None:
-            self.logOutput.Lines.Add("Now analysing link...")
-            yt = YouTube(self.txtEdit.Text)
-            yt.register_on_progress_callback(self.__on_download_Progress)
-            streams = yt.streams.filter(file_extension = "mp4").order_by("resolution").desc()
-            self.btnDownload.Caption = "Download '{a}'".format(a = yt.title)
+        self.logOutput.Lines.Add("Now analysing link...")
+        self.YT = YouTube(self.txtEditUrl.Text)
+        self.YT.register_on_progress_callback(self.__on_download_Progress)
+        streams = self.YT.streams.filter(file_extension = "mp4").order_by("resolution").desc()
+        self.btnDownload.Caption = "Download '{a}'".format(a = self.YT.title)
 
-            for stream in streams:
-                ##self.cmbBxVideos.Items.Add(stream)##.itag, "Resolution: {a}, FPS: {b}, Title: {c}".format(a = stream, b = stream, c = yt.title))
-                self.cmbBxVideos.Items.Add("Resolution: {a}, FPS: {b}".format(a = stream.resolution, b = stream.fps))
-                ##self.cmbBxVideos.Items.Add(stream.itag, "Resolution: {a}, FPS: {b}, Title: {c}".format(a = stream.resolution, b = stream.fps, c = yt.title))
+        for stream in streams:
+            self.cmbBxVideos.Items.Add("Resolution: {a}, FPS: {b}, {d} {c}".format(a = stream.resolution, b = stream.fps, c = stream.itag, d = self.StreamID))
 
-            self.logOutput.Lines.Add("Video to download is '{a}'".format(a = yt.title))
-            self.logOutput.Lines.Add("Analysis complete")
+        self.logOutput.Lines.Add("Video to download is '{a}'".format(a = self.YT.title))
+        self.logOutput.Lines.Add("Analysis complete")
+
+    def __on_cmbBxVideos_Change(self, sender) -> None:
+        searchString = self.StreamID
+        itagList = self.cmbBxVideos.Text.split(searchString)
+        itag = itagList[1].replace(searchString, "").strip()
+        self.logOutput.Lines.Add("Stream selected: {}".format(itag))
+        self.ITag = itag
 
     def __on_btnDownload_Click(self, sender) -> None:
-        try:
-            self.__download_video()
-        except RuntimeError as error:
-            self.logOutput.Lines.Add("Error with threading:" + error)
+        isValid = os.path.isfile(self.txtEditSave.Text)
 
+        if isValid:
+            self.__download_video()
+        else:
+            self.logOutput.Lines.Add("Please save file to a valid location")
 
     def __download_video(self) -> None:
-        self.logOutput.Lines.Add("Starting download...")
+        self.logOutput.Lines.Add("Starting download for '{a} ({b})'...".format(a = self.YT.title, b = self.ITag))
 
         try:
             ##stream = yt.streams.get_by_itag(22)
             ##stream.download()
-            yt = YouTube(self.txtEdit.Text)
-            yt.register_on_progress_callback(self.__on_download_Progress)
-            video = yt.streams.filter(progressive = True, file_extension = "mp4").last()
+##            yt = YouTube(self.txtEditUrl.Text)
+##            yt.register_on_progress_callback(self.__on_download_Progress)
+##            video = yt.streams.filter(progressive = True, file_extension = "mp4").last()
+##            video.download(filename = self.txtEditSave.Text)
+            video = self.YT.streams.get_by_itag(self.ITag)
+            ##self.logOutput.Lines.Add(video.itag)
             video.download(filename = self.txtEditSave.Text)
-            self.logOutput.Lines.Add("Download complete for " + yt.title)
+            self.logOutput.Lines.Add("Download complete for '{a}'".format(a = self.YT.title))
         except RuntimeError as error:
-            self.logOutput.Lines.Add("Error with link: " + error)
+            self.logOutput.Lines.Add("Error downloading: " + error)
 
     def __on_btnSave_Click(self, sender) -> None:
-        filename = filedialog.asksaveasfilename(initialdir = "%USERPROFILE%", initialfile = self.ytTitle, title = "Save file...", filetypes = (("MPEG-4 Video files","*.m4v"),("MPEG-4 Video files","*.mp4"),("all files","*.*")), defaultextension = ".m4v")
+        filename = filedialog.asksaveasfilename(initialdir = "%USERPROFILE%", initialfile = self.YT.title, title = "Save file '{a}'".format(a = self.YT.title), filetypes = (("MPEG-4 Video files","*.m4v"),("MPEG-4 Video files","*.mp4"),("all files","*.*")), defaultextension = ".m4v")
 
         if not (filename.endswith(".m4v") or filename.endswith(".mp4")):
             filename += ".m4v"
